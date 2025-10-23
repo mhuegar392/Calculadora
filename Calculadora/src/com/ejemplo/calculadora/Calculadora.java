5553180956668b53bbc9ec005913549bec459b13
mhuegar392 <mhuegar392@g.educaand.es>
2025-10-23 12:10:25 +0200

    version 1.5 multiplicar

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -1,8 +1,8 @@
 package com.ejemplo.calculadora;
 
 public class Calculadora {
-	public double restar(double a, double b) {
-		return a - b;
+	public double multiplicar(double a, double b) {
+		return a * b;
 
 	}
 }
