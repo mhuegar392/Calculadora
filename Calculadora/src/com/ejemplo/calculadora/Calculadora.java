03acb22994cc0f7214ff33cfafbddd18784b6d7a
mhuegar392 <mhuegar392@g.educaand.es>
2025-10-23 12:15:47 +0200

    1.6 division

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -1,8 +1,26 @@
 package com.ejemplo.calculadora;
 
 public class Calculadora {
+	
+	public double sumar(double a, double b) {
+		return a + b;
+
+	}
+	
+	public double restar(double a, double b) {
+		return a - b;
+	}
+	
 	public double multiplicar(double a, double b) {
 		return a * b;
+
+	}
+	
+	public double dividir(double a, double b) {
+		if (b == 0) {
+			throw new IllegalArgumentException("No se puede dividir entre cero");
+		} 
+		return a/b;
 
 	}
 }
